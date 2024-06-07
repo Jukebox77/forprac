e510481e0b88f59a9b5803de7dd101927d034209
Jukebox77 <noonzzang@naver.com>
2024-06-07 15:43:58 +0900

    silmu final

--- 6_silmu.py
+++ 6_silmu.py
@@ -41,32 +41,63 @@ file = os.listdir()[0]
 
 ############################################################
 
+
 누계 = 0
 for i in os.listdir():
-    hwp.Open(os.path.join(os.getcwd()), i)
+    hwp.Open(os.path.join(os.getcwd(), i))
     sleep(0.5)
-    hwp.PutFieldText("월당", str(랜덤(1, 10)))
-    누계 += int(hwp.GetFieldText("월당"))
-    hwp.PutFieldText("월누", "누계")
     
-    hwp.PutFieldText("화당", str(랜덤(1, 10)))
-    누계 += int(hwp.GetFieldText("화당"))
-    hwp.PutFieldText("화누", "누계")
+    hwp.PutFieldText("월당", 랜덤(1, 10))
+    월당 = hwp.GetFieldText("월당")
+    누계 += int(월당) if 월당.isdigit() else 0
+    hwp.PutFieldText("월누", 누계)
     
-    hwp.PutFieldText("수당", str(랜덤(1, 10)))
-    누계 += int(hwp.GetFieldText("수당"))
-    hwp.PutFieldText("수누", "누계")
+    hwp.PutFieldText("화당", 랜덤(1, 10))
+    화당 = hwp.GetFieldText("화당")
+    누계 += int(화당) if 화당.isdigit() else 0
+    hwp.PutFieldText("화누", 누계)
     
-    hwp.PutFieldText("목당", str(랜덤(1, 10)))
-    누계 += int(hwp.GetieldText("목당"))
-    hwp.PutFieldText("목누", "누계")
+    hwp.PutFieldText("수당", 랜덤(1, 10))
+    수당 = hwp.GetFieldText("수당")
+    누계 += int(수당) if 수당.isdigit() else 0
+    hwp.PutFieldText("수누", 누계)
     
-    hwp.PutFieldText("금당", str(랜덤(1, 10)))
-    누계 += int(hwp.GetFieldText("금당"))
-    hwp.PutFieldText("금누", "누계")
+    hwp.PutFieldText("목당", 랜덤(1, 10))
+    목당 = hwp.GetFieldText("목당")
+    누계 += int(목당) if 목당.isdigit() else 0
+    hwp.PutFieldText("목누", 누계)
+    
+    hwp.PutFieldText("금당", 랜덤(1, 10))
+    금당 = hwp.GetFieldText("금당")
+    누계 += int(금당) if 금당.isdigit() else 0
+    hwp.PutFieldText("금누", 누계)
+    
     hwp.Save()
     sleep(0.5)
     hwp.Run("FieldClose")
+
+
+startdate = dt.datetime(2024, 1, 1)
+delta = 0
+# 디렉토리의 파일들을 순회
+for file in os.listdir():
+    file_path = os.path.join(os.getcwd(), file)
+    hwp.Open(file_path)
+    for 요일 in "월화수목금토일":
+        nextdate = startdate + dt.timedelta(days=delta)
+
+        # 필드에 날짜 입력
+        hwp.PutFieldText(요일, nextdate.strftime("%y. %m. %d.") + f"({'월화수목금토일'[nextdate.weekday()]})")
+        delta += 1
+
+        # 파일 저장
+        hwp.Save()
+        sleep(0.05)
+    
+    # 파일 닫기
+    hwp.Run("FileClose")
+    # 충분한 시간 대기
+    # sleep(0.05)
 
 ############################################################
 
